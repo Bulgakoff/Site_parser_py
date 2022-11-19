77d32b0b2abeb93203cc46053c9fcc04c7388698
Alexei <hlbu@yandex.ru>
2022-11-19 17:01:00 +0300

    three commit

--- o_O1.py
+++ o_O1.py
@@ -1,6 +1,5 @@
 from requests import Session
 from bs4 import BeautifulSoup as bs
-from time import sleep
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
@@ -14,23 +13,25 @@ result = work.post('https://quotes.toscrape.com/login',
                    headers=headers,
                    data=data,
                    allow_redirects=True)
-author_list = []
-for num_page in range(1, 11):
+text_list = []
+authors_list = []
+for num_page in range(1, 20):
     response_new = work.get(f'https://quotes.toscrape.com/page/{num_page}/', headers=headers)
     soup_new = bs(response_new.text, 'lxml')
-    res = soup_new.find_all('span', class_='text')
-    print(res[0])
+    res = soup_new.find_all('div', class_='quote')
     if res:
         print(num_page)
 
     if len(res) != 0:
         print('Enter into  if')
         for item_page in res:
-            print(item_page)
-            author_to_list = item_page.find('span', class_='text').text
-            author_list.append(author_to_list)
+            author_text_to_list = item_page.find('span', class_='text').text
+            text_list.append(author_text_to_list)
+            author_to_list = item_page.find('small', class_='author').text
+            authors_list.append(author_to_list)
+
     else:
-        print(f'End Pages on {num_page} page !!!!!!!!!!!!!!!!!')
         break
 
-print(author_list)
+for i in range(len(text_list)):
+    print(text_list[i] + '\n' + authors_list[i] + '\n')
